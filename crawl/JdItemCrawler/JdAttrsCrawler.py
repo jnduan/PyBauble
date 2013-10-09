7c1821f0bc120c67ae9a19751a583125ee0f0a91
jnduan <jnduan@gmail.com>
2013-10-09 19:04:51 +0800

    抓取三级类目下商品的属性，并按照名值对存为json

---
+++ crawl/JdItemCrawler/JdAttrsCrawler.py
@@ -0,0 +1,69 @@
+__author__ = 'jnduan'
+import codecs
+import urllib2
+import math
+import sys
+import json
+from bs4 import BeautifulSoup
+
+
+def fetch_item_list_page(pno):
+    itemListUrl = itemListUrlPrefix + str(pno) + itemListUrlSuffix
+    print itemListUrl
+    itemListHtmlContent = opener.open(itemListUrl).read()
+    return itemListHtmlContent
+
+
+def extract_item_attr(pno):
+    itemListHtmlContent = fetch_item_list_page(pno)
+    itemListSoup = BeautifulSoup(itemListHtmlContent)
+    pNameDiv = itemListSoup.find_all("div", class_="p-name")
+    if pNameDiv:
+        for itemUrlDiv in pNameDiv:
+            itemUrl = itemUrlDiv.a.get("href")
+            print itemUrl
+            itemHtmlContent = opener.open(itemUrl).read()
+            itemSoup = BeautifulSoup(itemHtmlContent)
+            attrTable = itemSoup.find(id="product-detail-2")
+            if attrTable:
+                attrDict = {}
+                for attrTR in attrTable.find_all("tr"):
+                    td = attrTR.find_all("td")
+                    if len(td) == 2:
+                        attrDict[td[0].get_text()] = td[1].get_text()
+                attrJSON = json.dumps(attrDict)
+                output_file.write(attrJSON)
+                output_file.write("\n")
+
+
+def main(category_l1, category_l2, category_l3):
+    global opener, itemListUrlPrefix, itemListUrlSuffix, output_file, page_size
+    opener = urllib2.build_opener()
+    page_size = 36.0 #this is a magic number \(-_-)/
+    itemListUrlPrefix = 'http://list.jd.com/' + category_l1 + '-' + category_l2 + '-' + category_l3 + '-0-0-0-0-0-0-0-1-1-'
+    itemListUrlSuffix = '-1-1-72-33.html'
+    output_file_name = '/Users/jnduan/temp/jditems_attrs_' + category_l3 + '.txt'
+    print "save to %s" % (output_file_name)
+    output_file = codecs.open(output_file_name, 'w', 'utf-8')
+
+    #fetch 1st page and get the total items count, calc page count.
+    itemListFirstPageHtmlContent = fetch_item_list_page(1)
+    itemListFirstPageSoup = BeautifulSoup(itemListFirstPageHtmlContent)
+    filterDiv = itemListFirstPageSoup.find(id="filter")
+    totalDiv = filterDiv.find("div", class_="total")
+    itemCount = int(totalDiv.span.strong.get_text())
+
+    pageCount = int(math.ceil(itemCount / page_size))
+    print "total %d pages" % (pageCount)
+    pass
+    for pno in range(1, pageCount + 1):
+        extract_item_attr(pno)
+    output_file.close()
+
+
+if __name__ == '__main__':
+    if (len(sys.argv) == 4):
+        main(sys.argv[1], sys.argv[2], sys.argv[3])
+    else:
+        print "usage: python " + sys.argv[0] + " category_l1 cateogry_l2 category_l3"
+        sys.exit()
